3b4c8fd3ce900ff5f76290b70939b275490487e1
Simon Dewulf <simon_dewulf@hotmail.com>
2023-08-26 23:19:32 +0200

    fix beans

--- beans/src/main/java/be/ordina/beans/service/ChessGameServiceImpl.java
+++ beans/src/main/java/be/ordina/beans/service/ChessGameServiceImpl.java
@@ -1,25 +1,31 @@
 package be.ordina.beans.service;
 
 import be.ordina.beans.model.Game;
+import be.ordina.beans.model.Location;
+import be.ordina.beans.model.Piece;
 import be.ordina.beans.model.Player;
 import be.ordina.beans.repo.ChessGameRepository;
-import be.ordina.beans.repo.ChessGameRepositoryImpl;
+import org.springframework.stereotype.Component;
 
-import static be.ordina.beans.config.Config.board;
+import java.util.Map;
 
+@Component
 public class ChessGameServiceImpl implements ChessGameService {
 
     private final ChessGameRepository chessGameRepository;
+    private final Map<Location, Piece> board;
 
-    public ChessGameServiceImpl(ChessGameRepository chessGameRepository) {
+    public ChessGameServiceImpl(ChessGameRepository chessGameRepository,
+                                Map<Location, Piece> board) {
         this.chessGameRepository = chessGameRepository;
+        this.board = board;
     }
 
     public Game createGame() {
         var game = Game.builder()
                 .white(new Player("Julia"))
                 .black(new Player("Romeo"))
-                .board(board())
+                .board(board)
                 .build();
         return chessGameRepository.saveGame(game);
     }
